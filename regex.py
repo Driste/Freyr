8415e885670d63d81e49ebcd88de5c862c1fd858
Ginger <tyler.carper@g2-inc.com>
2016-03-01 21:43:44 -0500

    moved everything around

---
+++ regex.py
@@ -0,0 +1,141 @@
+import re
+
+# !IMPORTANT - don't forget about the multi-line "\" (backslash).
+
+header = ("action", "protocol", "source_address", "source_port", "direction", "destination_address", "destination_port")
+
+actions = ("alert", "log", "pass", "activate", "dynamic", "drop", "reject", "sdrop")
+protocols = ("tcp", "udp", "icnp", "ip")
+
+rule_header = re.compile(
+    r"^(?P<comment>#)*\s*"          # Commented or not
+    r"(?P<action>alert|log|pass|activate|dynamic|drop|reject|sdrop)\s*"     # Action
+    r"(?P<protocol>tcp|udp|icmp|ip)\s*"                                     # Protocol
+    r"(?P<saddress>[^\s]*)\s*"      # Source address (1-255.1-255.1-255.1-255/1-255 | lists of ips ex. [192.1.1.1/5,10.254.32.1/5]
+    #r"(?P<sport>[^\s]*)\s*"         # Source port (1-65535 | (1-65535|):(1-65535|) | any)
+    r"(?P<sport>any|[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])\s*"         # Source ports (1-65535 | (1-65535|):(1-65535|) | any)
+    r"(?P<direction>[-><]+)\s*"     # Direction <- | <> | ->
+    r"(?P<daddress>[^\s]*)\s*"      # Destination address
+    r"(?P<dport>[^\s]*)\s*"         # Destination port (1-65535 | (1-65535|):(1-65535|) | any)
+    r"\((?P<options>.*)\)\s*"       # Options within ()s
+)
+
+# These options provide information about the rule but do not have any affect during detection
+generalOptions = {
+    "msg" : re.compile("msg\s*:\s*\"(?P<msg>.*?)\";"),
+    "reference" : re.compile(""),
+    "gid" : re.compile(""),
+    "sid" : re.compile(""),
+    "rev" : re.compile(""),
+    "classtype" : re.compile(""),
+    "priority" : re.compile(""),
+    "metadata" : re.compile("")
+}
+
+
+# These options all look for data inside the packet payload and can be inter-related
+payloadDetection = {
+    "content" : re.compile(""),
+    "protected_content" : re.compile(""),
+    "hash" : re.compile(""),
+    "offset" : re.compile(""),
+    "length" : re.compile(""),
+    "rawbytes" : re.compile(""),
+    "depth" : re.compile(""),
+    "distance" : re.compile(""),
+    "within" : re.compile(""),
+    "http_client_body" : re.compile(""),
+    "http_cookie" : re.compile(""),
+    "http_raw_cookie" : re.compile(""),
+    "http_header" : re.compile(""),
+    "http_raw_header" : re.compile(""),
+    "http_raw_header" : re.compile(""),
+    "http_uri" : re.compile(""),
+    "http_raw_uri" : re.compile(""),
+    "http_stat_code" : re.compile(""),
+    "http_stat_msg" : re.compile(""),
+    "http_encode" : re.compile(""),
+    "fast_pattern" : re.compile(""),
+    "uricontent" : re.compile(""),
+    "urilen" : re.compile(""),
+    "isdataat" : re.compile(""),
+    "pcre" : re.compile(""),
+    "pkt_data" : re.compile(""),
+    "file_data" : re.compile(""),
+    "base64_decode": re.compile(""),
+    "base64_data" : re.compile(""),
+    "byte_test" : re.compile(""),
+    "byte_jump" : re.compile(""),
+    "byte_extract" : re.compile(""),
+    "ftpbounce" : re.compile(""),
+    "asn1" : re.compile(""),
+    "cvs" : re.compile(""),
+    "dce_iface" : re.compile(""),
+    "dce_opnum" : re.compile(""),
+    "dce_stub_data" : re.compile(""),
+    "sip_method" : re.compile(""),
+    "sip_stat_code" : re.compile(""),
+    "sip_header" : re.compile(""),
+    "sip_body" : re.compile(""),
+    "gtp_type" : re.compile(""),
+    "gtp_info" : re.compile(""),
+    "gtp_version" : re.compile(""),
+    "ssl_version" : re.compile(""),
+    "ssl_state" : re.compile("")
+}
+
+
+# These options look for non-payload data
+nonpayloadDetection = {
+    "fragoffset" : re.compile(""),
+    "ttl" : re.compile(""),
+    "tos" : re.compile(""),
+    "id" : re.compile(""),
+    "ipopts" : re.compile(""),
+    "fragbits" : re.compile(""),
+    "dsize" : re.compile(""),
+    "flags" : re.compile(""),
+    "flow" : re.compile(""),
+    "flowbits" : re.compile(""),
+    "seq" : re.compile(""),
+    "ack" : re.compile(""),
+    "window" : re.compile(""),
+    "itype" : re.compile(""),
+    "icode" : re.compile(""),
+    "icmp_id" : re.compile(""),
+    "cmp_seq" : re.compile(""),
+    "rpc" : re.compile(""),
+    "ip_proto" : re.compile(""),
+    "sameip" : re.compile(""),
+    "stream_reassemble" : re.compile(""),
+    "stream_size" : re.compile("")
+}
+
+
+# These options are rule specific triggers that happen after a rule has fired.
+postDetection = {
+    "logto" : re.compile(""),
+    "session" : re.compile(""),
+    "resp" : re.compile(""),
+    "react" : re.compile(""),
+    "tag" : re.compile(""),
+    "activates" : re.compile(""),
+    "activated_by" : re.compile(""),
+    "count" : re.compile(""),
+    "replace" : re.compile(""),
+    "detection_filter": re.compile("")
+}
+
+# Make sure all the options are valid
+checkOptions = {
+
+}
+
+# Check between all the options to make sure there is nothing unknown
+checkGutters = {
+
+}
+
+
+
+
--- validate.py
+++ validate.py
@@ -1,66 +1,50 @@
 import re
+import regex
 
-# !IMPORTANT - don't forget about the multi-line "\" (backslash).
-
-header = ("action", "protocol", "source_address", "source_port", "direction", "destination_address", "destination_port")
-actions = ("alert", "log", "pass", "activate", "dynamic", "drop", "reject", "sdrop")
-protocols = ("tcp", "udp", "icnp", "ip")
-
-rule_header = re.compile(
-    r"^(?P<comment>#)*\s*"          # Commented or not
-    r"(?P<action>alert|log|pass|activate|dynamic|drop|reject|sdrop)\s*"     # Action
-    r"(?P<protocol>tcp|udp|icmp|ip)\s*"                                     # Protocol
-    r"(?P<saddress>[^\s]*)\s*"      # Source addresses (1-255.1-255.1-255.1-255/1-255 | lists of ips ex. [192.1.1.1/5,10.254.32.1/5]
-    #r"(?P<sport>[^\s]*)\s*"         # Source ports (1-65535 | (1-65535|):(1-65535|) | any)
-    r"(?P<sport>any|[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])\s*"         # Source ports (1-65535 | (1-65535|):(1-65535|) | any)
-    r"(?P<direction>[-><]+)\s*"     # Direction <- | <> | ->
-    r"(?P<daddress>[^\s]*)\s*"      # Destination addresses
-    r"(?P<dport>[^\s]*)\s*"         # Destination ports (1-65535 | (1-65535|):(1-65535|) | any)
-    r"\((?P<options>.*)\)\s*"       # Options within ()s
-)
-
-# These options provide information about the rule but do not have any affect during detection
-general_options = ("msg", "reference", "gid", "sid", "rev", "classtype", "priority", "metadata")
-
-# These options all look for data inside the packet payload and can be inter-related
-payload_detection = ("content", "protected_content", "hash", "offset", "length", "rawbytes", "depth", "distance", 
-                     "within", "http_client_body", "http_cookie", "http_raw_cookie", "http_header", "http_raw_header",
-                     "http_raw_header", "http_uri", "http_raw_uri", "http_stat_code", "http_stat_msg", "http_encode",
-                     "fast_pattern", "uricontent", "urilen", "isdataat", "pcre", "pkt_data", "file_data", "base64_decode",
-                     "base64_data", "byte_test", "byte_jump", "byte_extract", "ftpbounce", "asn1", "cvs", "dce_iface", 
-                     "dce_opnum", "dce_stub_data", "sip_method", "sip_stat_code", "sip_header", "sip_body", "gtp_type",
-                     "gtp_info", "gtp_version", "ssl_version", "ssl_state")
-
-# These options look for non-payload data
-nonpayload_detect = ("fragoffset", "ttl", "tos", "id", "ipopts", "fragbits", "dsize", "flags", "flow", "flowbits", "seq",
-                     "ack", "window", "itype", "icode", "icmp_id", "cmp_seq", "rpc", "ip_proto", "sameip", "stream_reassemble",
-                     "stream_size")
-
-# These options are rule specific triggers that happen after a rule has fired.
-post_detection = ("logto", "session", "resp", "react", "tag", "activates", "activated_by", "count", "replace", "detection_filter")
-
-# body pattern
-rule_options = re.compile(
-    r""     
-)
-
-myFile = open("rules/community.rules")
-rules = []
-
-# bring the lines into memory
-for line in myFile:
-    rules.append(line)
+class Rule(dict):
+    ''' breaks the rule into its parts along with validating it '''
     
-# go through each rule
-for rule in rules[0:8]:
-    if re.match(rule_header, rule):
-        #print re.match(rule_head, rule).group('sport')
-        print ""
-    else:
-        print "fail"
+    def __init__(self, rule):
+        ''' self['valid'] will be changed at any point to show whether the rule is valid or not. Error will tell you where.'''
+        self['valid'] = True
+        self['error'] = None
+        self['rawRule'] = rule
+        
+        self.header()
+    
+    def __getattr__(self, i):
+        ''' Get any value from the dict '''
+        return self[i]
+    
+    def header(self):
+        ''' maps the header options to self'''
+        
+        if re.match(regex.rule_header, self['rawRule']):
+            header = re.match(regex.rule_header, self['rawRule']).groupdict()
+            for option in header:
+                self[option] = header[option]
+        else:
+            self['valid'] = False
+    
+    def generalOptions(self):
+        pass
+    
+    def payloadDetection(self):
+        pass
+    
+    def nonpayloadDetection(self):
+        pass
+    
+    def postDetection(self):
+        pass
 
-
-
-
-
-
+if __name__ == "__main__":
+    
+    myFile = open("rules/community.rules")
+    
+    i = 0
+    rule = {}
+    for line in myFile:
+        rule[i] = Rule(line)
+        print rule[i].valid
+        i += 1
